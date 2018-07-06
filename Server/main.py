e4f3ae6c0633c8ba1d0ba48170786e87bc0d370f
miraedbswo <miraedbswo@gmail.com>
2018-07-06 12:58:00 +0900

    create_app이라는 새로운 개념 도입.  db.init_app으로 나중에 init 해줌.

---
+++ Server/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from api import route
+import config
+
+db = SQLAlchemy()
+
+
+def create_app(*config_obj):
+    app_ = Flask(__name__)
+
+    for obj in config_obj:
+        app_.config.from_object(obj)
+
+    db.init_app(app_)
+    app_
+    .config['db'] = db
+
+    route(app_)
+
+    return app_
+
+
+app = create_app(config.Config())
