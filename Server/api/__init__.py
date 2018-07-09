5a267126ac1d0872448fb735cad40b9b7196ec65
miraedbswo <miraedbswo@gmail.com>
2018-07-09 23:46:43 +0900

    UPDATE: 로깅 함수 지정해줌

--- Server/api/__init__.py
+++ Server/api/__init__.py
@@ -1,3 +1,25 @@
+from flask import Flask, g
+import logging
+from logging.handlers import RotatingFileHandler
+
+from api import user
+
+
 def route(app):
-    from api import user
     app.register_blueprint(user.api.blueprint)
+
+
+def log(app):
+    @app.before_request
+    def before_first_request():
+        def make_logger():
+            handler = RotatingFileHandler('server_log.log', maxBytes=10000, backupCount=2)
+            formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
+            handler.setFormatter(formatter)
+            app.logger.addHandler(handler)
+
+        make_logger()
+        g.logger = app.logger
+        print(g.logger)
+
+        g.logger.info('<===server logging===>')
