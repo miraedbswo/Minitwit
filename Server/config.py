f1136a96960e7be773a8c162abacacba2fa9fd9f
miraedbswo <miraedbswo@gmail.com>
2018-07-09 23:45:45 +0900

    UPDATE: jwt_secret_key 넣음

--- Server/config.py
+++ Server/config.py
@@ -1,3 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///Database/Database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///Models/Models.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'secret'
