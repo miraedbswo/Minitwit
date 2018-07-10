8a85836fcff9b21c785431797abd5415610d8e39
miraedbswo <miraedbswo@gmail.com>
2018-07-10 23:55:41 +0900

    UPDATE: SECRET_KEY

--- Server/config.py
+++ Server/config.py
@@ -1,4 +1,6 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///Models/Models.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'secret'
+    JWT_SECRET_KEY = 'IAmJWTSecretKey'
+    SECRET_KEY = 'IAmSecretKey'
+
