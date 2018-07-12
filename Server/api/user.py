1bdeffdb2d8e9a817850987b167665bb850e1c27
miraedbswo <miraedbswo@gmail.com>
2018-07-12 23:57:07 +0900

    UPDATE: url_prefix 수정 & etc

--- Server/api/user.py
+++ Server/api/user.py
@@ -1,9 +1,10 @@
-from flask import Blueprint, abort, current_app, request, g, Response
+from flask import Blueprint, abort, current_app, request
 from flask_restful import Api, Resource
 from flask_jwt_extended import create_access_token, create_refresh_token
 
 from Models.account import UserModel
-import json
+
+from api import get_200_response
 
 blueprint = Blueprint('user_blueprint', __name__, url_prefix='/user')
 api = Api(blueprint)
@@ -20,7 +21,6 @@ class Signup(Resource):
             return {'msg': '잘못된 입력값입니다'}
 
         if UserModel.query.filter_by(id=body['id']).first():
-            g.logger.info('Register failed')
             abort(409)
 
         user = UserModel(id=body['id'], pw=body['pw'], username=body['username'])
@@ -41,15 +41,11 @@ class Login(Resource):
             return {'msg': '잘못된 입력값입니다.'}
 
         if UserModel.query.filter_by(id=body['id'], pw=body['pw']).first() is not None:
-            return Response(
-                json.dumps({
-                    'access_token': create_access_token(identity=body['id']),
-                    'refresh_token': create_refresh_token(identity=body['pw'])
-                }, ensure_ascii=False),
-                status=200,
-                content_type='application/json;charset=utf-8'
-            )
+            return get_200_response({
+                'access_token': create_access_token(identity=body['id']),
+                'refresh_token': create_refresh_token(identity=body['pw'])
+            })
         else:
             return {
                 'msg': '아이디 또는 비밀번호가 틀렸습니다.'
-            }
+            },
