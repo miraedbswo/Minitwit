59e54a354ba6014db12c3423bc0102313f4cd4db
miraedbswo <miraedbswo@gmail.com>
2018-07-17 23:55:09 +0900

    [UPDATE] : login

--- Server/app/views/user.py
+++ Server/app/views/user.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, abort, request, Response
 from flask_restful import Api, Resource
 from flask_jwt_extended import create_access_token, create_refresh_token
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from app.models.account import UserModel
 from app.views import get_200_response
@@ -13,9 +13,10 @@ api.prefix = '/user'
 
 @api.resource('/signup')
 class Signup(Resource):
+    def get(self):
+        return 'hi', 200
 
     def post(self):
-
         payload = request.json
         if not payload:
             abort(406)
@@ -37,7 +38,40 @@ class Signup(Resource):
 
         return Response('', 201)
 
+
+@api.resource('/login')
+class Login(Resource):
+    def get(self):
+        return 'hi', 200
+
+    def post(self):
+        payload = request.json
+
+        user = UserModel.objects(id=payload['id'], pw=check_password_hash())
+        # pw 잘 모르겠음. 추후 추가 예정
+
+
+# @api.resource('/login')
+# class Login(Resource):
+#     def get(self):
+#         return 'Hi Login', 200
 #
+#     def post(self):
+#         body = request.json
+#         if not body:
+#             return {'msg': '잘못된 입력값입니다.'}
+#
+#         if UserModel.query.filter_by(id=body['id'], pw=body['pw']).first() is not None:
+#             return get_200_response({
+#                 'access_token': create_access_token(identity=body['id']),
+#                 'refresh_token': create_refresh_token(identity=body['pw'])
+#             })
+#         else:
+#             return {
+#                 'msg': '아이디 또는 비밀번호가 틀렸습니다.'
+#             },
+
+
 # @api.resource('/register')
 # class Register(Resource):
 #     def get(self):
@@ -56,24 +90,3 @@ class Signup(Resource):
 #         current_app.config['db'].session.commit()
 #
 #         return '', 201
-
-
-@api.resource('/login')
-class Login(Resource):
-    def get(self):
-        return 'Hi Login', 200
-
-    def post(self):
-        body = request.json
-        if not body:
-            return {'msg': '잘못된 입력값입니다.'}
-
-        if UserModel.query.filter_by(id=body['id'], pw=body['pw']).first() is not None:
-            return get_200_response({
-                'access_token': create_access_token(identity=body['id']),
-                'refresh_token': create_refresh_token(identity=body['pw'])
-            })
-        else:
-            return {
-                'msg': '아이디 또는 비밀번호가 틀렸습니다.'
-            },
