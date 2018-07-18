c9d3e80d52d53db109459138d3cc536a38b137d2
miraedbswo <miraedbswo@gmail.com>
2018-07-18 23:59:22 +0900

    [DELETE] : 기존 CONFIG 파일 제거

--- Server/config.py
+++
@@ -1,6 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///Models/Models.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'IAmJWTSecretKey'
-    SECRET_KEY = 'IAmSecretKey'
-
