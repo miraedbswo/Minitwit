fafca9bb6678364a5096ae5697867b9aef809b66
miraedbswo <miraedbswo@gmail.com>
2018-07-18 23:58:29 +0900

    [UPDATE] : log 함수 제거

--- Server/app/__init__.py
+++ Server/app/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask_jwt_extended import JWTManager
 from mongoengine import connect
-import logging
-from logging.handlers import RotatingFileHandler
 
 
 def route(app_):
@@ -10,22 +8,6 @@ def route(app_):
     app_.register_blueprint(user.api.blueprint)
     from app.views import post
     app_.register_blueprint(post.api.blueprint)
-
-
-def log(app):
-    @app.before_request
-    def before_first_request():
-        def make_logger():
-            handler = RotatingFileHandler('server_log.log', maxBytes=10000, backupCount=2)
-            formatter = logging.Formatter("[%(asctime)s] %(levelname)s - %(message)s")
-            handler.setFormatter(formatter)
-            app.logger.addHandler(handler)
-
-        make_logger()
-        g.logger = app.logger
-        print(g.logger)
-
-        g.logger.info('server logging')
 
 
 def create_app(*config_obj):
@@ -39,7 +21,6 @@ def create_app(*config_obj):
     # app_.config['db'] = db
 
     route(app_)
-    log(app_)
 
     JWTManager().init_app(app_)
 
