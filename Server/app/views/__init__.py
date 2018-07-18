192dde99e5ff682d25acd5652e384a490cfbedba
miraedbswo <miraedbswo@gmail.com>
2018-07-18 23:59:10 +0900

    [UPDATE] : unicode safe 하는 함수를 BaseResource로 바꾸어줌

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -1,12 +1,14 @@
 from flask import g, Response
+from flask_restful import Resource
 import json
 
 
-def get_200_response(data):
-    return Response(
-        json.dumps(data, ensure_ascii=False),
-        status=200,
-        content_type='application/json;charset=utf-8'
-    )
+class BaseResource(Resource):
 
-
+    @classmethod
+    def unicode_safe_json_dumps(cls, data):
+        return Response(
+            json.dumps(data, ensure_ascii=False),
+            status=200,
+            content_type='application/json; charset=utf8'
+        )
