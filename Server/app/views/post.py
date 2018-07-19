54b75e5367dcf5df19466328f8828083cbef6849
miraedbswo <miraedbswo@gmail.com>
2018-07-19 22:49:39 +0900

    [ING] : 작업 중 1/N

--- Server/app/views/post.py
+++ Server/app/views/post.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, abort, request
+from flask import Blueprint, abort, request, redirect, url_for
 from flask_restful import Api
 from flask_jwt_extended import jwt_required
+from mongoengine import *
 
 from app.views import BaseResource
 from app.models.post import Comment, PostModel
-
 
 blueprint = Blueprint(__name__, __name__)
 api = Api(blueprint)
@@ -12,18 +12,23 @@ api.prefix = '/post'
 
 
 @api.resource('/')
-class PostCombine(BaseResource):
+class HandleRequests(BaseResource):
+    @jwt_required
     def get(self):
-        return self.unicode_safe_json_dumps({
-            'msg': '한글 유니코드 안 깨지는지 테스트'
-        })
+        for post in PostModel.objects().all():
+            return self.unicode_safe_json_dumps({
+                'title': post.title,
+                'author': post.author,
+                'content': post.content,
+                'comments': post.comments,
+                'uptime': post.uptime
+            })
 
     def post(self):
-        pass
-        # post 한 내용 모두 다 띄워주는 기능
+        return redirect(url_for(Posting))
 
 
-@api.resource('/')
+@api.resource('/submit')
 class Posting(BaseResource):
     @jwt_required
     def post(self):
