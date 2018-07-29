862bf8ea226c611a9319875f1f5fcd88907cadc3
miraedbswo <miraedbswo@gmail.com>
2018-07-29 23:39:45 +0900

    [UPDATE] : nickname과 datetime을 게시물에 넣음

--- Server/app/views/post.py
+++ Server/app/views/post.py
@@ -3,9 +3,11 @@ from flask_restful import Api
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from mongoengine import *
 
+from datetime import datetime
+
 from app.views import BaseResource
 from app.models.account import UserModel
-from app.models.post import Comment, PostModel
+from app.models.post import PostModel
 
 blueprint = Blueprint(__name__, __name__)
 api = Api(blueprint)
@@ -38,19 +40,24 @@ class Posting(BaseResource):
         if not payload:
             abort(409)
 
-        title = payload['title']
-        content = payload['content']
+        title = payload["title"]
+        content = payload["content"]
 
         user = UserModel.objects(id=get_jwt_identity()).first()
+        if user is None:
+            abort(406)
 
         PostModel(
             title=title,
             author=user.name,
-            content=content
+            content=content,
+            timestamp=datetime.now().__str__()
         ).save()
 
         return self.unicode_safe_json_dumps({
-            "msg": "게시물 작성이 완료되었습니다."
+            "msg": "게시물 작성이 완료되었습니다.",
+            "nickname": user.name,
+            "datetime": datetime.now().__str__()
         }, 201)
 
 
