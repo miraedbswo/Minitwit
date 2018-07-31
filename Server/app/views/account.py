7016f43d11be8ba766983c33b6ece790855f30ee
miraedbswo <miraedbswo@gmail.com>
2018-07-31 20:12:55 +0900

    [ADD] : 비밀번호 변경 기능 추가

--- Server/app/views/account.py
+++ Server/app/views/account.py
@@ -1,75 +1,39 @@
-from flask import Blueprint, Response, abort, request
+from flask import abort, request, Blueprint
 from flask_restful import Api
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import get_jwt_identity, jwt_required
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from app.models.account import UserModel
 from app.views import BaseResource
+
 
 blueprint = Blueprint(__name__, __name__)
 api = Api(blueprint)
 api.prefix = '/user'
 
 
-@api.resource('/signup')
-class Signup(BaseResource):
-    def get(self):
-        return 'hi', 200
-
+@api.resource('/change-pw')
+class ChangePW(BaseResource):
+    @jwt_required
     def post(self):
-        payload = request.json
-        if not payload:
-            abort(406)
 
-        id = payload['id']
-        pw = payload['pw']
-        name = payload['name']
-        email = payload['email']
-
-        if UserModel.objects(id=id):
-            return self.unicode_safe_json_dumps({
-                "msg": '중복된 id 값입니다.'
-            }, 409)
-
-        try:
-            hashed_pw = generate_password_hash(pw)
-
-            UserModel(
-                id=id,
-                pw=hashed_pw,
-                name=name,
-                email=email
-            ).save()
-
-        except TypeError:
-            abort(400)
-
-        return Response('회원가입 완료', 201)
-
-
-@api.resource('/login')
-class Login(BaseResource):
-    def get(self):
-        return 'hi', 200
-
-    def post(self):
         payload = request.json
 
-        if not payload:
-            abort(406)
+        current_pw = payload['current_pw']
+        new_pw = payload['new_pw']
 
-        user_id = payload['id']
-        user_pw = payload['pw']
+        user = UserModel.objects(id=get_jwt_identity()).first()
 
-        user = UserModel.objects(id=user_id).first()
+        self.check_user_is_exist(user)
 
-        if user is None:
-            abort(406)
+        if not check_password_hash(user.pw, current_pw):
+            abort(403)
 
-        if check_password_hash(user.pw, user_pw):
-            return {
-                'access_token': create_access_token(identity=user_id),
-                'refresh_token': create_refresh_token(identity=user_id)
-            }
-        else:
-            abort(406)
+        if current_pw == new_pw:
+            abort(409)
+
+        user.update(pw=generate_password_hash(new_pw))
+
+        return self.unicode_safe_json_dumps({
+            "msg": f"비밀번호가 {new_pw}로 변경되었습니다.",
+        }, 200)
