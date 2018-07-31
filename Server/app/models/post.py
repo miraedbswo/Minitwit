f65f2a7f71772146d2d4aeeb031a657a51d9529c
miraedbswo <miraedbswo@gmail.com>
2018-07-31 21:45:31 +0900

    Comment 그냥 안할거\

--- Server/app/models/post.py
+++ Server/app/models/post.py
@@ -1,22 +1,5 @@
 from mongoengine import *
 
-from datetime import datetime
-
-
-class Comment(EmbeddedDocument):
-    content = StringField()
-    # comment의 content
-
-    author = ReferenceField(
-        document_type="UserModel"
-    )
-    # 작성자
-
-    timestamp = StringField(
-        default=datetime.now
-    )
-    # 덧글 단 시간
-
 
 class PostModel(Document):
 
@@ -25,17 +8,11 @@ class PostModel(Document):
     )
     # Post의 제목
 
-    author = StringField(
-    )
+    author = StringField()
     # 작성자
 
     content = StringField()
     # 작성할 글
-
-    comments = ListField(
-        EmbeddedDocumentListField(Comment)
-    )
-    # 덧글
 
     timestamp = StringField()
     # Post 올라온 시간
