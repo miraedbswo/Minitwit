59356cc8b101d628dc8f748e3953b351ed1446b7
miraedbswo <miraedbswo@gmail.com>
2018-08-23 16:30:02 +0900

    Router class 구현.

--- Server/app/views/__init__.py
+++ Server/app/views/__init__.py
@@ -8,6 +8,12 @@ class BaseResource(Resource):
 
     @classmethod
     def unicode_safe_json_dumps(cls, data, status_code=200):
+        """
+        json 형식에서 utf-8 형식 한글이 깨지는 현상 방지.
+        한글을 json 으로 보낼 때 unicode_safe_json_dumps를 사용한다.
+
+        :param data: json type (dict)
+        """
         return Response(
             json.dumps(data, ensure_ascii=False),
             status_code,
@@ -20,10 +26,21 @@ class BaseResource(Resource):
             abort(406)
 
 
-def router(app_):
-    from app.views import auth
-    app_.register_blueprint(auth.api.blueprint)
-    from app.views import post
-    app_.register_blueprint(post.api.blueprint)
-    from app.views import account
-    app_.register_blueprint(account.api.blueprint)
+class Router:
+    """
+    기능 별 blueprint 들을 모아서 register 해주는 class 구현.
+
+    :param app: A flask application
+    """
+
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        from app.views import auth
+        app.register_blueprint(auth.api.blueprint)
+        from app.views import post
+        app.register_blueprint(post.api.blueprint)
+        from app.views import account
+        app.register_blueprint(account.api.blueprint)
