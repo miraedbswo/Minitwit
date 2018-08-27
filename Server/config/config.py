921f5bebfe6eecd773e6d9c9e0b6961081305363
miraedbswo <miraedbswo@gmail.com>
2018-08-27 23:26:00 +0900

    test flasgger

--- Server/config/config.py
+++ Server/config/config.py
@@ -23,6 +23,44 @@ class Config:
         'password': os.getenv('MONGO_PW')
     }
 
+    SWAGGER = {
+        'title': SERVICE_NAME,
+        'uiversion': 3,
+
+        'info': {
+            'title': SERVICE_NAME + ' API',
+            'version': '1.0',
+            'description': 'test'
+        },
+
+        'host': 'localhost',
+        'basePath': '/api'
+    }
+
+    SWAGGER_TEMPLATE = {
+        'schemes': [
+            'http'
+        ],
+        'tags': [
+            {
+                'name': '[User] 계정',
+                'description': '계정 관련 API'
+            },
+            {
+                'name': '[User] 계정 관리',
+                'description': '계정 관리 관련 API'
+            },
+            {
+                'name': '[User] 게시글',
+                'description': '모든 권한으로 접근 가능한 게시글 관련 API'
+            },
+            {
+                'name': '[User] 게시글 관리',
+                'description': '게시글 관리 관련 API'
+            }
+        ]
+    }
+
     # SQLALCHEMY_DATABASE_URI = "sqlite:///models/models.db"
     # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
