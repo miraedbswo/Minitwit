56d5e70c9d8385774cc7bd258ec1aff6fa4c00c1
miraedbswo <miraedbswo@gmail.com>
2018-09-03 16:09:11 +0900

    insert secret_key warning

--- Server/run_server.py
+++ Server/run_server.py
@@ -1,7 +1,13 @@
+import os
+
 from app import create_app
 from config.config import Config
 
-app = create_app(Config)
 
 if __name__ == '__main__':
+    app = create_app(Config)
+
+    if 'SECRET_KEY' not in os.environ:
+        print('[WARN] SECRET KEY is not set in the environment variable !!')
+
     app.run(**app.config['RUN_SETTINGS'])
