635c6e45eef35e60dc87a29d65c3821b8182e2f3
miraedbswo <miraedbswo@gmail.com>
2018-09-12 23:57:15 +0900

    add BaseModel

--- Server/app/models/__init__.py
+++ Server/app/models/__init__.py
@@ -1,2 +1,14 @@
+from mongoengine import *
+
+from datetime import datetime
 
 
+class BaseModel(Document):
+    meta = {
+        'allow_inheritance': True,
+        'abstract': True
+    }
+
+    creation_time = DateTimeField(
+        default=datetime.now()
+    )
