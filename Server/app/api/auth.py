95e4deaa5bf04b2d336676a9416073ac2f40f410
Goming <miraedbswo@gmail.com>
2018-09-16 23:47:20 +0900

    fix refresh func

--- Server/app/api/auth.py
+++ Server/app/api/auth.py
@@ -70,5 +70,4 @@ class GetRefreshToken(BaseResource):
 
         return {
             'access_token': create_access_token(identity=user.id),
-            'refresh_token': create_refresh_token(identity=user.id)
         }
