a75248ad2eee871de19c52d7566317042343cbbd
Goming <miraedbswo@gmail.com>
2018-09-19 23:48:54 +0900

    add FollowModel

---
+++ Server/app/models/follow.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+
+from mongoengine import *
+
+from app.models.account import UserModel
+
+
+class FollowModel(Document):
+    """
+    follower = 팔로우 하는 사람
+    followee = 팔로우 받는 사람
+    """
+    follower = ReferenceField(
+        UserModel
+    )
+
+    followee = ReferenceField(
+        UserModel
+    )
+
+    follow_time = DateTimeField(
+        default=datetime.now
+    )
