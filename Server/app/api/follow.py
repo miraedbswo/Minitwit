23f3dd8a09fcb252394b2584ef5b02a8d35c29a5
Goming <miraedbswo@gmail.com>
2018-09-19 23:49:13 +0900

    add Follow viewfunctions

--- Server/app/api/follow.py
+++ Server/app/api/follow.py
@@ -1,22 +1,53 @@
-from flask import Blueprint
+from flask import Blueprint, Response
 from flask_restful import Api
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from mongoengine.queryset.visitor import Q
 
-from app.api import BaseResource, json_required
-from app.models.account import UserModel, FollowModel
+from app.api import BaseResource, get_user_inform
+from app.models.account import UserModel
+from app.models.follow import FollowModel
 
 blueprint = Blueprint(__name__, __name__)
 api = Api(blueprint)
+api.prefix = 'follow'
 
 
-@api.resource('/follower')
-class Followers(BaseResource):
-    @jwt_required
-    def get(self):
-        pass
+@api.resource('/<username>')
+class Follow(BaseResource):
+    @get_user_inform
+    def get(self, username):
+        owner = UserModel.objects(name=username).first()
+        followers = FollowModel.objects(followee=owner).all()
+        following = FollowModel.objects(follower=owner).all()
 
-    @jwt_required
-    def post(self):
-        account = UserModel.objects(id=get_jwt_identity()).first()
+        return self.unicode_safe_json_dumps({
+            "followers": [follower.name for follower in followers.follower],
+            "following": [following.name for following in following.followee]
+        })
 
-        pass
+    @get_user_inform
+    def post(self, username):
+        owner = UserModel.objects(name=username).first()
+        self.check_is_exist(owner)
+
+        if not FollowModel.objects(Q(follower=g.user) & Q(followee=owner)).first():
+            return Response('', 208)
+
+        FollowModel(
+            follower=g.user,
+            followee=owner
+        ).save()
+
+        return Response('', 201)
+
+    @get_user_inform
+    def delete(self, username):
+        owner = UserModel.objects(name=username).first()
+        self.check_is_exist(owner)
+
+        follow = FollowModel.objects(Q(follower=g.user) & Q(followee=owner)).first()
+        self.check_is_exist(follow)
+
+        follow.delete()
+
+        return Response('', 200)
+
