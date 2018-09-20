675f1035951fe829ea2842b6ecc78b7f447bd64b
Goming <miraedbswo@gmail.com>
2018-09-20 22:35:32 +0900

    Fix uri path parameter

--- Server/app/api/follow.py
+++ Server/app/api/follow.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, Response
+from flask import Blueprint, Response, g
 from flask_restful import Api
 from mongoengine.queryset.visitor import Q
 
@@ -8,28 +8,34 @@ from app.models.follow import FollowModel
 
 blueprint = Blueprint(__name__, __name__)
 api = Api(blueprint)
-api.prefix = 'follow'
+api.prefix = '/follow'
 
 
-@api.resource('/<username>')
+@api.resource('/<nickname>')
 class Follow(BaseResource):
-    @get_user_inform
-    def get(self, username):
-        owner = UserModel.objects(name=username).first()
+    def get(self, nickname):
+        owner = UserModel.objects(nickname=nickname).first()
+        self.check_is_exist(owner)
+
         followers = FollowModel.objects(followee=owner).all()
         following = FollowModel.objects(follower=owner).all()
+        if not followers or following:
+            return Response('', 204)
+
+        followers_list = [follow_model.follower.nickname for follow_model in followers]
+        following_list = [follow_model.followee.nickname for follow_model in following]
 
         return self.unicode_safe_json_dumps({
-            "followers": [follower.name for follower in followers.follower],
-            "following": [following.name for following in following.followee]
+            "followers": followers_list,
+            "following": following_list
         })
 
     @get_user_inform
-    def post(self, username):
-        owner = UserModel.objects(name=username).first()
+    def post(self, nickname):
+        owner = UserModel.objects(nickname=nickname).first()
         self.check_is_exist(owner)
 
-        if not FollowModel.objects(Q(follower=g.user) & Q(followee=owner)).first():
+        if FollowModel.objects(Q(follower=g.user) & Q(followee=owner)).first():
             return Response('', 208)
 
         FollowModel(
@@ -40,8 +46,8 @@ class Follow(BaseResource):
         return Response('', 201)
 
     @get_user_inform
-    def delete(self, username):
-        owner = UserModel.objects(name=username).first()
+    def delete(self, nickname):
+        owner = UserModel.objects(nickname=nickname).first()
         self.check_is_exist(owner)
 
         follow = FollowModel.objects(Q(follower=g.user) & Q(followee=owner)).first()
