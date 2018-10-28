bc2c9a00b6bebc2bef006a7b3f394b25b4b996de
Goming <miraedbswo@gmail.com>
2018-10-28 22:30:52 +0900

    Fix Delete comment api

--- Server/app/api/post/comment.py
+++ Server/app/api/post/comment.py
@@ -7,16 +7,16 @@ from app.models.post import CommentModel, PostModel
 class CommentView(BaseResource):
     @get_user_inform
     @json_required({'comment': str})
-    def post(self, obj_id, comment_num):
+    def post(self, obj_id):
         post = PostModel.objects(id=obj_id).first()
-        self.check_is_exist(post, comment_num)
+        self.check_is_exist(post)
 
-        new_comment_num = len(post.comments) + 1
+        comment_num = len(post.comments) + 1
 
         comment = CommentModel(
             user=g.user,
             comment=request.json['comment'],
-            comment_num=new_comment_num
+            comment_num=comment_num
         )
 
         post.comments.append(comment)
@@ -25,14 +25,15 @@ class CommentView(BaseResource):
         return Response('', 201)
 
     @get_user_inform
-    def delete(self, obj_id, comment_num):
+    def delete(self, obj_id):
         post = PostModel.objects(id=obj_id).first()
+        comment_num = int(request.args.get('comment_num'))
         self.check_is_exist(post, comment_num)
 
         if not len(post.comments):
             return Response('', 204)
 
-        comment = PostModel.objects(comment__match={"comment_num": comment_num}).first()
-        comment.delete()
+        PostModel.objects(comments__match={"comment_num": comment_num}) \
+                 .update_one(pull__comments_comment_num=comment_num)
 
-        return Response('', 200)
+        return '', 200
