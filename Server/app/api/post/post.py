404f19e52741b29b28d836a4a0c0f6b2e7d21bec
Goming <miraedbswo@gmail.com>
2018-11-05 12:05:51 +0900

    input swagger + fix error

--- Server/app/api/post/post.py
+++ Server/app/api/post/post.py
@@ -1,10 +1,13 @@
 from flask import Response, abort, request, g
+from flasgger import swag_from
 
 from app.api import BaseResource, get_user_inform, json_required
 from app.models.post import PostModel
+from app.docs.post.post import SHOW_ALL_NOTICE_GET, WRITE_NOTICE_POST, ONE_NOTICE_GET, ONE_NOTICE_DELETE
 
 
 class ShowAllPostView(BaseResource):
+    @swag_from(SHOW_ALL_NOTICE_GET)
     @get_user_inform
     def get(self):
         all_post = PostModel.objects().all()
@@ -17,15 +20,16 @@ class ShowAllPostView(BaseResource):
             'author': post.author.nickname,
             'content': post.content,
             'comments': [{
-                "name": data.name,
+                "name": data.user.name,
                 "comment": data.comment
-            } for data in post.comments],
+            } for data in post.comments if data],
             'tags': post.tags,
             'timestamp': str(post.timestamp)
         } for post in all_post], 200)
 
 
 class WritePostView(BaseResource):
+    @swag_from(WRITE_NOTICE_POST)
     @get_user_inform
     @json_required({'title': str, 'content': str})
     def post(self):
@@ -53,6 +57,7 @@ class WritePostView(BaseResource):
 
 
 class OnePostView(BaseResource):
+    @swag_from(ONE_NOTICE_GET)
     @get_user_inform
     def get(self, obj_id):
         post = PostModel.objects(id=obj_id).first()
@@ -71,6 +76,7 @@ class OnePostView(BaseResource):
             'timestamp': str(post.timestamp)
         }, 200)
 
+    @swag_from(ONE_NOTICE_DELETE)
     @get_user_inform
     def delete(self, obj_id):
         post = PostModel.objects(id=obj_id).first()
